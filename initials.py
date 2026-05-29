f0284957f758dc9a354befe8790e3d229e71dd50
leonard-2105 <2425402339@live.gctu.edu.gh>
2026-05-29 06:17:12 -0700

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: LYD.py
+# Description:Prints my initials 
+# Assignment Number:  1
+#
+# Name: Dzivenu Leonard
+# STUDENT ID:2425402339
+# Email: dzivenuleonard50@gmail.com
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Dzivenu Leonard this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
+    # This program printts my  initials in stylized large letters
+  
+    print()
+    print("...LYD")
+    print()
+    print("...LLLL................YY........YY........DDDDDDDD.........")
+    print("...LLLL................YY........YY........DDDDDDDDDD.......")
+    print("...LLLL.................YY......YY.........DD......DDDD.....")
+    print("...LLLL..................YY....YY..........DD........DD.....")
+    print("...LLLL...................YYYYYY...........DD........DD.....")
+    print("...LLLL....................YYYY............DD........DD.....")
+    print("...LLLL....................YYYY............DD........DD.....")
+    print("...LLLL....................YYYY............DD......DDDD.....")
+    print("...LLLLLLLLLLLL...**.......YYYY.......**...DDDDDDDDDD.....**")
+    print("...LLLLLLLLLLLL...**.......YYYY.......**...DDDDDDDD.......**")
+    print()
+ 
 
 
 main()
